21f6b2cb43998e917d99097b46bea66f93529a61
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-02-19 09:54:22 -0800

    successfully fail ADD_BEER test

--- src/_tests_/reducers/beer-list-reducer.test.js
+++ src/_tests_/reducers/beer-list-reducer.test.js
@@ -1,7 +1,43 @@
 import beerListReducer from '../../reducers/beer-list-reducer';
 
 describe('beerListReducer', () => {
+
+  let action;
+  const beerData = {
+    name: 'Dumpster Fire IPA',
+    brand: 'End Of The World Brewing',
+    price: ' $5.00',
+    alcoholContent: '6%',
+    pints: 124,
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(beerListReducer({}, { type: null})).toEqual({});
   });
+
+  test('Should successfully add new beer data to masterBeerList', () => {
+    const { name, brand, price, alcoholContent, pints, id } = beerData;
+    action = {
+      type: 'ADD_BEER',
+      name,
+      brand,
+      price,
+      alcoholContent,
+      pints,
+      id
+    };
+
+    expect(beerListReducer({}, action)).toEqual({
+      [id] : {
+        name,
+        brand,
+        price,
+        alcoholContent,
+        pints,
+        id
+      }
+    });
+  });
+
 });
--- src/reducers/beer-list-reducer.js
+++ src/reducers/beer-list-reducer.js
@@ -1,3 +1,7 @@
 export default (state = {}, action) => {
+  // const { name, brand, price, alcoholContent, pints, id } = action;
+  // switch (action.type) {
+  //   case 'ADD_BEER'
+  // }
   return state;
 };
