94bf6dfedae1e2ef43b4494dc3c169ce550efc29
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-02-19 10:11:40 -0800

    successfully fail DELETE_BEER test

--- src/_tests_/reducers/beer-list-reducer.test.js
+++ src/_tests_/reducers/beer-list-reducer.test.js
@@ -2,6 +2,21 @@ import beerListReducer from '../../reducers/beer-list-reducer';
 
 describe('beerListReducer', () => {
 
+  const currentState = {
+    1: {name: 'Dumpster Fire IPA',
+    brand: 'End Of The World Brewing',
+    price: ' $5.00',
+    alcoholContent: '6%',
+    pints: 124,
+    id: 1},
+    2: {name: 'Rainy Day Ale',
+    brand: 'PDX Brewing',
+    price: '$5.00',
+    alcoholContent: '6%',
+    pints: 124,
+    id: 2}
+  }
+
   let action;
   const beerData = {
     name: 'Dumpster Fire IPA',
@@ -37,6 +52,21 @@ describe('beerListReducer', () => {
         pints,
         id
       }
+    });
+  });
+
+  test('Should successfully delete a beer', () => {
+    action = {
+      type: 'DELETE_BEER',
+      id: 1
+    };
+    expect(beerListReducer(currentState, action)).toEqual({
+      2: {name: 'Rainy Day Ale',
+      brand: 'PDX Brewing',
+      price: '$5.00',
+      alcoholContent: '6%',
+      pints: 124,
+      id: 2 }
     });
   });
 
